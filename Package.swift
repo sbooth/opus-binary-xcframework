a42feb63cce427854e4a66232a7fc33b6d66d205
Stephen Booth <me@sbooth.org>
2026-02-13 07:25:57 -0600

    Update opus to version 1.6.1

--- Package.swift
+++ Package.swift
@@ -23,7 +23,7 @@ let package = Package(
 		// Targets can depend on other targets in this package and products from dependencies.
 		.binaryTarget(
 			name: "opus",
-			url: "https://github.com/sbooth/opus-binary-xcframework/releases/download/0.2.2/opus.xcframework.zip",
-			checksum: "a7c0549b4d45e3304ee3c7d975b9a4ec6b02d8367284a34e1e438d944534a63d"),
+			url: "https://github.com/sbooth/opus-binary-xcframework/releases/download/0.3.0/opus.xcframework.zip",
+			checksum: "bae1b26f2e0e3e45c9fe001b73302d7cc9cb0f8a8db8f75956c87e2bb062a73e"),
 	]
 )
